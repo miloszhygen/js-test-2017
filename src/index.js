9c9b7c69b6eeba2b03208b97bf4118e2eb165c0a
Milosz <miloszhygen@gmail.com>
2017-02-05 22:37:44 +0100

    Added react-redux & -router
    Added routing to main index and moved the App.js to components folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import { Provider } from 'react-redux';
 import store from './store';
-import App from './App';
+import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
